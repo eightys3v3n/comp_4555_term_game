66c252800bf84414c16bad4c6aa98b7446490188
eightys3v3n <eightys3v3n@gmail.com>
2022-12-11 10:32:06 -0700

    Switch between main menu and playing states

--- src/system/main_menu_inputs.rs
+++ src/system/main_menu_inputs.rs
@@ -1,7 +1,11 @@
 use bevy::{
     prelude::*,
+    input::{
+        keyboard::KeyboardInput,
+        ButtonState,
+    },
 };
-use log::warn;
+use log::{ warn, info };
 use super::super::component::*;
 use super::super::{
     resource::{ config::Config },
@@ -12,14 +16,34 @@ pub fn handle_main_menu_inputs(
     keys: Res<Input<KeyCode>>,
     config: Res<Config>,
     mut state: ResMut<State<AppState>>,
+    mut keyboard_events: EventReader<KeyboardInput>,
 ) {
     if state.current() != &AppState::MainMenu { return; }
 
-    if keys.just_pressed(KeyCode::Escape) {
-        info!("Switching to playing.");
-        match state.set(AppState::Playing) {
-            Ok(v) => {},
-            Err(e) => warn!("Failed to switch into the plaing state on Escape pressed"),
+    for event in keyboard_events.iter() {
+        match event.state {
+            ButtonState::Pressed => {
+                match event.key_code {
+                    Some(key_code) => {
+                        if key_code == KeyCode::Escape {
+                            match state.set(AppState::Playing) {
+                                Ok(v) => info!("Switched into Playing state"),
+                                Err(e) => warn!("Failed to switch into the playing state on Escape pressed"),
+                            }
+                        }
+                    }
+                    None => {}
+                }
+            },
+            ButtonState::Released => {}
         }
     }
+
+    // if keys.just_pressed(KeyCode::Escape) {
+    //     info!("Switching to playing.");
+    //     match state.set(AppState::Playing) {
+    //         Ok(v) => {},
+    //         Err(e) => warn!("Failed to switch into the plaing state on Escape pressed"),
+    //     }
+    // }
 }
--- src/system/playing_inputs.rs
+++ src/system/playing_inputs.rs
@@ -1,5 +1,9 @@
 use bevy::{
     prelude::*,
+    input::{
+        keyboard::KeyboardInput,
+        ButtonState,
+    },
 };
 use log::warn;
 use super::super::component::*;
@@ -12,14 +16,34 @@ pub fn handle_playing_inputs(
     keys: Res<Input<KeyCode>>,
     config: Res<Config>,
     mut state: ResMut<State<AppState>>,
+    mut keyboard_events: EventReader<KeyboardInput>,
 ) {
     if state.current() != &AppState::Playing { return; }
 
-    if keys.just_pressed(KeyCode::Escape) {
-        info!("Switching to main menu.");
-        match state.set(AppState::MainMenu) {
-            Ok(v) => {},
-            Err(e) => warn!("Failed to switch into the main menu state on Escape pressed"),
+    for event in keyboard_events.iter() {
+        match event.state {
+            ButtonState::Pressed => {
+                match event.key_code {
+                    Some(key_code) => {
+                        if key_code == KeyCode::Escape {
+                            match state.set(AppState::MainMenu) {
+                                Ok(v) => info!("Switched into Main Menu state"),
+                                Err(e) => warn!("Failed to switch into the main menu state on Escape pressed"),
+                            }
+                        }
+                    }
+                    None => {}
+                }
+            },
+            ButtonState::Released => {}
         }
     }
+
+    // if keys.just_pressed(KeyCode::Escape) {
+    //     info!("Switching to main menu.");
+    //     match state.set(AppState::MainMenu) {
+    //         Ok(v) => {},
+    //         Err(e) => warn!("Failed to switch into the main menu state on Escape pressed"),
+    //     }
+    // }
 }
