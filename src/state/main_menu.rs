68def812c41082fa2fbe21f50b1455c13dc70200
eightys3v3n <eightys3v3n@gmail.com>
2022-12-11 11:25:03 -0700

    Create two buttons using bevy built-in button

--- src/state/main_menu.rs
+++ src/state/main_menu.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    ui::UiImage,
+};
 use super::super::{
     resource::{
         config::Config,
@@ -16,24 +19,80 @@ pub fn setup(
 ) {
     let window = windows.get_primary().unwrap();
 
-    info!("Displaying buttons at {}x{}", window.width() / 2.0 - config.menu.r#continue.width, 0.0);
+    // commands.spawn((
+    //     Button,
+    //     SpriteBundle {
+    //         texture: asset_server.load(&config.menu.new_game.image_path),
+    //         transform: Transform::from_xyz(0.0, 0.0, config.menu.new_game.z_height),
+    //         sprite: Sprite {
+    //             custom_size: Some(Vec2::new(config.menu.new_game.width, config.menu.new_game.height)),
+    //             ..default()
+    //         },
+    //         ..default()
+    //     },
+    // ));
+    // commands.spawn((
+    //     Button,
+    //     SpriteBundle {
+    //         texture: asset_server.load(&config.menu.exit.image_path),
+    //         transform: Transform::from_xyz(0.0, -100.0, config.menu.exit.z_height),
+    //         sprite: Sprite {
+    //             custom_size: Some(Vec2::new(config.menu.exit.width, config.menu.exit.height)),
+    //             ..default()
+    //         },
+    //         ..default()
+    //     },
+    // ));
 
-    commands.spawn((
-        Button,
-        SpriteBundle {
-            texture: asset_server.load(&config.menu.r#continue.image_path),
-            transform: Transform::from_xyz(window.width() / 2.0 - config.menu.r#continue.width / 2.0, 0.0, config.menu.r#continue.z_height),
+    commands
+        .spawn(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(config.menu.new_game.width), Val::Px(config.menu.new_game.height)),
+                // center button
+                margin: UiRect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            image: UiImage(asset_server.load(&config.menu.new_game.image_path)),
+            background_color: Color::rgb(5.0, 5.0, 5.0).into(),
             ..default()
-        },
-        // SpriteBundle {
-        //     texture: asset_server.load(&config.menu.new_game.image_path),
-        //     transform: Transform::from_xyz(window.width() / 2.0 - config.menu.new_game.width / 2.0, 0.0, config.menu.new_game.z_height),
-        //     ..default()
-        // },
-        // SpriteBundle {
-        //     texture: asset_server.load(&config.menu.exit.image_path),
-        //     transform: Transform::from_xyz(window.width() / 2.0 - config.menu.exit.width / 2.0, 0.0, config.menu.exit.z_height),
-        //     ..default()
-        // },
-    ));
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                &config.menu.new_game.text,
+                TextStyle {
+                    font_size: 40.0,
+                    color: Color::rgb(0.0, 0.0, 0.0),
+                    font: asset_server.load(&config.menu.button_font),
+                },
+            ));
+        });
+    commands
+        .spawn(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(config.menu.exit.width), Val::Px(config.menu.exit.height)),
+                // center button
+                margin: UiRect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            background_color: Color::rgb(5.0, 5.0, 5.0).into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                &config.menu.new_game.text,
+                TextStyle {
+                    font_size: 40.0,
+                    color: Color::rgb(0.0, 0.0, 0.0),
+                    font: asset_server.load(&config.menu.button_font),
+                },
+            ));
+        });
 }
--- src/system/main_menu_inputs.rs
+++ src/system/main_menu_inputs.rs
@@ -3,6 +3,7 @@ use bevy::{
     input::{
         keyboard::KeyboardInput,
         ButtonState,
+        mouse::MouseButtonInput,
     },
 };
 use log::{ warn, info };
@@ -12,32 +13,53 @@ use super::super::{
     enums::AppState,
 };
 
+
 pub fn handle_main_menu_inputs(
     keys: Res<Input<KeyCode>>,
     config: Res<Config>,
     mut state: ResMut<State<AppState>>,
     mut keyboard_events: EventReader<KeyboardInput>,
+    mut button_interaction: Query<
+        (&Interaction, &mut BackgroundColor, &Children),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut text_query: Query<&mut Text>,
 ) {
     if state.current() != &AppState::MainMenu { return; }
 
-    for event in keyboard_events.iter() {
-        match event.state {
-            ButtonState::Pressed => {
-                match event.key_code {
-                    Some(key_code) => {
-                        if key_code == KeyCode::Escape {
-                            match state.set(AppState::Playing) {
-                                Ok(v) => info!("Switched into Playing state"),
-                                Err(e) => warn!("Failed to switch into the playing state on Escape pressed"),
-                            }
-                        }
-                    }
-                    None => {}
-                }
-            },
-            ButtonState::Released => {}
+    for (interaction, mut color, children) in &mut button_interaction {
+        let mut text = text_query.get_mut(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                text.sections[0].value = "Press".to_string();
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Hover".to_string();
+            }
+            Interaction::None => {
+                text.sections[0].value = "Button".to_string();
+            }
         }
     }
+
+    // for event in keyboard_events.iter() {
+    //     match event.state {
+    //         ButtonState::Pressed => {
+    //             match event.key_code {
+    //                 Some(key_code) => {
+    //                     if key_code == KeyCode::Escape {
+    //                         match state.set(AppState::Playing) {
+    //                             Ok(v) => info!("Switched into Playing state"),
+    //                             Err(e) => warn!("Failed to switch into the playing state on Escape pressed"),
+    //                         }
+    //                     }
+    //                 }
+    //                 None => {}
+    //             }
+    //         },
+    //         ButtonState::Released => {}
+    //     }
+    // }
 
     // if keys.just_pressed(KeyCode::Escape) {
     //     info!("Switching to playing.");
@@ -46,4 +68,6 @@ pub fn handle_main_menu_inputs(
     //         Err(e) => warn!("Failed to switch into the plaing state on Escape pressed"),
     //     }
     // }
+
+
 }
