9bb87ef83b0880bfedbf024dc68d0699c3b87a49
eightys3v3n <eightys3v3n@gmail.com>
2022-12-12 19:11:39 -0700

    Protect against no enemies or players

--- src/system/player_movement.rs
+++ src/system/player_movement.rs
@@ -11,48 +11,50 @@ pub fn set_player_movements(
     config: Res<Config>,
     mut query: Query<&mut Velocity, With<Player>>,
 ) {
-    match query.get_single_mut() {
-        Ok(mut velocity) => {
-            let (mut vel_x, mut vel_y) = velocity.to_xy();
+    if ! query.is_empty() {
+        match query.get_single_mut() {
+            Ok(mut velocity) => {
+                let (mut vel_x, mut vel_y) = velocity.to_xy();
 
-            if keys.just_pressed(KeyCode::W) {
-                info!("Pressed W");
-                vel_y += config.player.move_speed;
-            }
-            else if keys.just_released(KeyCode::W) {
-                info!("Released W");
-                vel_y -= config.player.move_speed;
-            }
+                if keys.just_pressed(KeyCode::W) {
+                    info!("Pressed W");
+                    vel_y += config.player.move_speed;
+                }
+                else if keys.just_released(KeyCode::W) {
+                    info!("Released W");
+                    vel_y -= config.player.move_speed;
+                }
 
-            if keys.just_pressed(KeyCode::S) {
-                info!("Pressed S");
-                vel_y -= config.player.move_speed;
-            }
-            else if keys.just_released(KeyCode::S) {
-                info!("Released S");
-                vel_y += config.player.move_speed;
-            }
+                if keys.just_pressed(KeyCode::S) {
+                    info!("Pressed S");
+                    vel_y -= config.player.move_speed;
+                }
+                else if keys.just_released(KeyCode::S) {
+                    info!("Released S");
+                    vel_y += config.player.move_speed;
+                }
 
-            if keys.just_pressed(KeyCode::A) {
-                info!("Pressed A");
-                vel_x -= config.player.move_speed;
-            }
-            else if keys.just_released(KeyCode::A) {
-                info!("Released A");
-                vel_x += config.player.move_speed;
-            }
+                if keys.just_pressed(KeyCode::A) {
+                    info!("Pressed A");
+                    vel_x -= config.player.move_speed;
+                }
+                else if keys.just_released(KeyCode::A) {
+                    info!("Released A");
+                    vel_x += config.player.move_speed;
+                }
 
-            if keys.just_pressed(KeyCode::D) {
-                info!("Pressed D");
-                vel_x += config.player.move_speed;
-            }
-            else if keys.just_released(KeyCode::D) {
-                info!("Released D");
-                vel_x -= config.player.move_speed;
-            }
+                if keys.just_pressed(KeyCode::D) {
+                    info!("Pressed D");
+                    vel_x += config.player.move_speed;
+                }
+                else if keys.just_released(KeyCode::D) {
+                    info!("Released D");
+                    vel_x -= config.player.move_speed;
+                }
 
-            velocity.from_xy(vel_x, vel_y);
-        },
-        Err(err) => warn!("Error finding player for movement: {}", err)
-    };
+                velocity.from_xy(vel_x, vel_y);
+            },
+            Err(err) => warn!("Error finding player for movement: {}", err)
+        };
+    }
 }
