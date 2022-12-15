87a883aa3d5a8a023fb17fe8e6e684025437316a
eightys3v3n <eightys3v3n@gmail.com>
2022-12-15 16:48:13 -0700

    Fix and tweak enemy movement speed

--- src/system/enemy_movement.rs
+++ src/system/enemy_movement.rs
@@ -4,12 +4,14 @@ use bevy::{
 use log::warn;
 use super::super::{
     component::*,
-    enums::MoveBehaviour,
+    enums::*,
+    resource::config::Config,
 };
 
 pub fn enemy_movement(
     player: Query<&Transform, (With<Player>, Without<Enemy>)>,
     mut enemies: Query<(&mut Transform, &Enemy, &mut Velocity), (With<Enemy>, Without<Player>)>,
+    config: Res<Config>,
 ) {
     for (mut enemy_transform, enemy, mut enemy_velocity) in enemies.iter_mut() {
         match enemy.move_behaviour {
@@ -22,6 +24,11 @@ pub fn enemy_movement(
 
                         enemy_transform.rotation = Quat::from_rotation_z(displ_vel.direction.to_radians());
                         enemy_velocity.direction = displ_vel.direction;
+
+                        match enemy.r#type {
+                            EnemyType::Basic => enemy_velocity.scalar = config.enemy.basic.move_speed,
+                            _ => warn!("Unimplemented enemy movement speed; moving some default amount"),
+                        }
                     }
                     Err(e) => warn!("Couldn't use the player's position to direct enemy movements because there is no player? {}", e)
                 }
