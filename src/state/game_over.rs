3b403a95262a6c3f7b4d9f3c2fc7d65457c570ee
eightys3v3n <eightys3v3n@gmail.com>
2022-12-17 11:10:24 -0700

    Reformat GameOver screen and add score

--- src/state/game_over.rs
+++ src/state/game_over.rs
@@ -4,6 +4,8 @@ use bevy::{
 use super::super::{
     resource::{
         config::Config,
+        round::RoundInfo,
+        counter::Counters,
     },
     component::*,
     ui::*,
@@ -14,6 +16,8 @@ pub fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     config: Res<Config>,
+    round: Res<RoundInfo>,
+    counters: Res<Counters>,
 ) {
     commands
         .spawn((
@@ -23,7 +27,7 @@ pub fn setup(
                     justify_content: JustifyContent::SpaceBetween,
                     ..default()
                 },
-                background_color: Color::rgb(0., 0., 1.).into(),
+                // background_color: Color::rgb(0., 0., 1.).into(),
                 ..default()
             },
             GameOver,
@@ -32,43 +36,80 @@ pub fn setup(
             parent.spawn((
                 NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Percent(60.0), Val::Percent(100.0)),
+                        size: Size::new(Val::Percent(30.0), Val::Percent(100.0)),
                         ..default()
                     },
-                    background_color: Color::rgb(1., 0., 0.).into(),
+                    // background_color: Color::rgb(1., 0., 0.).into(),
                     ..default()
                 },
             ));
             parent.spawn((
                 NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Percent(100.0), Val::Percent(30.0)),
+                        size: Size::new(Val::Percent(40.0), Val::Percent(100.0)),
                         flex_direction: FlexDirection::Column,
-                        // justify_content: JustifyContent::Center,
+                        justify_content: JustifyContent::SpaceAround,
+                        // align_content: AlignContent::Center,
                         ..default()
                     },
-                    background_color: Color::rgb(0., 1., 0.).into(),
+                    // background_color: Color::rgb(0., 1., 0.).into(),
                     ..default()
                 },
             ))
             .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "This would be your score!",
-                    TextStyle {
-                        font_size: 40.0,
-                        color: Color::rgb(0.0, 0.0, 0.0),
-                        font: asset_server.load(&config.menu.button_font),
+                parent.spawn(
+                    NodeBundle {
+                        style: Style {
+                            size: Size::new(Val::Percent(100.), Val::Px(100.)),
+                            flex_direction: FlexDirection::Column,
+                            ..default()
+                        },
+                        background_color: Color::rgba(0., 0., 0., 0.7).into(),
+                        ..default()
                     },
-                ));
-                spawn_button(parent, &config, &config.menu.main_menu, &asset_server);
+                )
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        format!("Made it to round {}", round.number),
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::rgb(1., 1., 1.),
+                            font: asset_server.load(&config.menu.button_font),
+                        }
+                    ).with_text_alignment(TextAlignment::CENTER)
+                    );
+                    parent.spawn(TextBundle::from_section(
+                        format!("Killed {} enemies", counters.enemies_killed),
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::rgb(1., 1., 1.),
+                            font: asset_server.load(&config.menu.button_font),
+                        },
+                    ));
+                });
+                parent.spawn(
+                    NodeBundle {
+                        style: Style {
+                            size: Size::new(Val::Percent(100.), Val::Px(100.)),
+                            flex_direction: FlexDirection::Column,
+                            justify_content: JustifyContent::SpaceBetween,
+                            ..default()
+                        },
+                        // background_color: Color::rgb(1., 1., 0.).into(),
+                        ..default()
+                    },
+                )
+                .with_children(|parent| {
+                    spawn_button(parent, &config, &config.menu.main_menu, &asset_server);
+                });
             });
             parent.spawn((
                 NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Percent(60.0), Val::Percent(100.0)),
+                        size: Size::new(Val::Percent(30.0), Val::Percent(100.0)),
                         ..default()
                     },
-                    background_color: Color::rgb(1., 0., 0.).into(),
+                    // background_color: Color::rgb(1., 0., 0.).into(),
                     ..default()
                 },
             ));
